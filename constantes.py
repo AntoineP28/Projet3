a1413b926637c403143f67a388f8bda303b0bb2f
ParisRP <56562335+ParisRPV2@users.noreply.github.com>
2020-05-28 11:45:38 +0200

    commit

---
+++ constantes.py
@@ -0,0 +1,22 @@
+"""Constantes du jeu de Labyrinthe"""
+
+#Paramètres de la fenêtre
+nombre_sprite_cote = 15
+taille_sprite = 30
+cote_fenetre = nombre_sprite_cote * taille_sprite
+
+#Personnalisation de la fenêtre
+titre_fenetre = "Goomba Labyrinthe"
+image_icone = "images/goomba_droite.png"
+
+#Listes des images du jeu
+image_accueil = "images/accueil.png"
+image_fond = "images/fond.jpg"
+image_mur = "images/mur.png"
+image_depart = "images/depart.png"
+image_arrivee = "images/arrivee.png"
+image_pièce = "images/pièce.png"
+image_potion = "images/potion.png"
+image_coffre = "images/coffre.png"
+image_victoire = "images/victoire.png"
+image_défaite = "images/Défaite.png"
